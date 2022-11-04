0ad30f2fd00b125bea7d12d93b98040aa03f1e51
omoloye13 <omoloyefauziyat@gmail.com>
2022-11-04 13:36:28 +0000

    Setup my work directory

---
+++ src/Component/Button.js
@@ -0,0 +1,83 @@
+import { Link } from "react-router-dom";
+export const TwitterButton = () => {
+  return (
+    <>
+      <a href="https://twitter.com/adunlomooye" id="twitter" className="btn">
+        Twitter Link
+      </a>
+    </>
+  );
+};
+
+export const ZuriButton = () => {
+  return (
+    <>
+      <a href="https://training.zuri.team/" id="btn__zuri" className="btn">
+        Zuri Team
+      </a>
+    </>
+  );
+};
+export const BookButton = () => {
+  return (
+    <>
+      <a
+        href="http://books.zuri.team"
+        id="books"
+        title="Zuri books"
+        className="btn"
+      >
+        Zuri Books
+      </a>
+    </>
+  );
+};
+export const PythonButton = () => {
+  return (
+    <>
+      <a
+        href="https://books.zuri.team/python-for-beginners?ref_id=<Fauziyat Folashade Omoloye>"
+        id="book__python"
+        title="Python books"
+        className="btn"
+      >
+        Python Books
+      </a>
+    </>
+  );
+};
+export const PitchButton = () => {
+  return (
+    <>
+      <a
+        href="https://background.zuri.team"
+        id="pitch"
+        title="background checks on coders"
+        className="btn"
+      >
+        Background Check for Coders
+      </a>
+    </>
+  );
+};
+export const DesignButton = () => {
+  return (
+    <>
+      <a
+        href="https://books.zuri.team/design-rules"
+        id="book__design"
+        className="btn"
+        title="free design book offered by Zuri"
+      >
+        Design Books
+      </a>
+    </>
+  );
+};
+export const ContactButton = () => {
+  return (
+    <>
+      <Link to="/contact" id="contact" className="btn">Contact Me</Link>
+    </>
+  );
+};
